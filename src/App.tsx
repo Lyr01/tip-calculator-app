c8feaff429fc30df358dd85b94601faf4ce2e8d5
Lyr01 <79974123+Lyr01@users.noreply.github.com>
2022-12-02 10:14:15 +0100

    html completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,44 @@ import "./App.css";
 
 function App() {
 	return (
-		<div className="App">
-			Bill Select Tip % 5% 10% 15% 25% 50% Custom Number of People Tip Amount /
-			person Total / person Reset
+		<main className="App">
+			<div className="container">
+				<div className="left-section">
+					<div className="bill">
+						<label htmlFor="bill">Bill</label>
+						<input type="number" id="bill" placeholder="0" />
+					</div>
+					<div className="select-tip">
+						<span>Select Tip %</span>
+						<button>5%</button>
+						<button>10%</button>
+						<button>15%</button>
+						<button>25%</button>
+						<button>50%</button>
+						<input type="number" placeholder="Custom" />
+					</div>
+					<div className="number-of-peoples">
+						<label htmlFor="number-of-people">Number of People</label>
+						<input type="number" id="number-of-people" placeholder="0" />
+					</div>
+				</div>
+				<div className="right-section">
+					<div>
+						<span>
+							Tip Amount <small>/ person</small>
+						</span>
+						<h1>$0.00</h1>
+					</div>
+					<div>
+						<span>
+							Total <small>/ person</small>
+						</span>
+						<h1>$0.00</h1>
+					</div>
+					<button>Reset</button>
+				</div>
+			</div>
+
 			{/* <div className="attribution">
 				Challenge by{" "}
 				<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
@@ -12,7 +47,7 @@ function App() {
 				</a>
 				. Coded by <a href="#">Your Name Here</a>.
 			</div> */}
-		</div>
+		</main>
 	);
 }
 
